9dad8093f21f909dc18730466cbaa5cc32faca1b
Adam Zionts <adam@meteor.com>
2017-12-19 16:50:50 -0800

    Move message logic out of meteor, into galaxy
    -Now the polling logic just looks for a message from the
    /version-status/ endpoint and logs the message when it changes. This
    gives Galaxy the ability to change what that message is without needing
    to change the Meteor tool
    -This change also adds a flag called --no-wait which allows users to
    specify that they want the deploy command to behave just as before

--- tools/cli/commands.js
+++ tools/cli/commands.js
@@ -1353,6 +1353,7 @@ main.registerCommand({
     'override-architecture-with-local' : { type: Boolean },
     'allow-incompatible-update': { type: Boolean },
     'deploy-polling-timeout': {type: Number},
+    'no-wait': { type: Boolean },
   },
   allowUnrecognizedOptions: true,
   requiresApp: function (options) {
@@ -1413,10 +1414,12 @@ main.registerCommand({
     serverArch: buildArch
   };
 
-  let deployPollingTimeoutMillis = null;
+  let deployPollingTimeoutMs = null;
   if (!!options['deploy-polling-timeout']) {
-    deployPollingTimeoutMillis = options['deploy-polling-timeout'];
+    deployPollingTimeoutMs = options['deploy-polling-timeout'];
   }
+
+  let waitForDeploy = !options['no-wait'];
 
   var deployResult = deploy.bundleAndDeploy({
     projectContext: projectContext,
@@ -1424,7 +1427,8 @@ main.registerCommand({
     settingsFile: options.settings,
     buildOptions: buildOptions,
     rawOptions,
-    deployPollingTimeoutMillis,
+    deployPollingTimeoutMs,
+    waitForDeploy,
   });
 
   if (deployResult === 0) {
